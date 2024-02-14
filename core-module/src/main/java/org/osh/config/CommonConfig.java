17f9bf83225f9cc1cab0264daabd87e451401607
오상현 <osh@osanghyeon-ui-MacBookAir.local>
2024-02-14 23:16:15 +0900

    feat: exclude actuator url from tracing

--- core-module/src/main/java/org/osh/config/CommonConfig.java
+++ core-module/src/main/java/org/osh/config/CommonConfig.java
@@ -1,26 +1,19 @@
 package org.osh.config;
 
+import io.micrometer.observation.ObservationPredicate;
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.context.Context;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.data.LinkData;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import io.opentelemetry.sdk.trace.samplers.Sampler;
-import io.opentelemetry.sdk.trace.samplers.SamplingResult;
 import io.opentelemetry.semconv.ResourceAttributes;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
 
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -43,7 +36,6 @@ public class CommonConfig {
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
                 .setResource(resource)
                 .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
-                .setSampler(customSampler())
                 .build();
 
         return OpenTelemetrySdk.builder()
@@ -51,36 +43,17 @@ public class CommonConfig {
                 .buildAndRegisterGlobal();
     }
 
-    private Sampler customSampler() {
-        //TODO: add sampling ratio properties
-        Sampler rootSampler = Sampler.traceIdRatioBased(1.0);
-        return new Sampler() {
-            @Override
-            public SamplingResult shouldSample(Context parentContext,
-                                               String traceId,
-                                               String name,
-                                               SpanKind spanKind,
-                                               Attributes attributes,
-                                               List<LinkData> parentLinks) {
-                // Check if the span is of kind SERVER, indicating an incoming request
-                if (spanKind == SpanKind.CLIENT) {
-                    // Use the correct attribute key to identify the request path or endpoint
-                    String httpRoute = attributes.get(AttributeKey.stringKey("http.url"));
-                    System.out.println(parentContext.toString());
-                    System.out.println(attributes);
-                    // If the route is for an actuator endpoint, ignore it
-                    if (httpRoute != null && httpRoute.startsWith("/actuator")) {
-                        return SamplingResult.drop();
-                    }
-                }
-                // For other spans, defer to the otelSampler
-                return rootSampler.shouldSample(parentContext, traceId, name, spanKind, attributes, parentLinks);
+    /**
+     * Custom ObservationPredicate Bean.
+     * actuator의 health check 를 observe 하지않게 설정.
+     */
+    @Bean
+    ObservationPredicate actuatorServerContextPredicate() {
+        return (name, context) -> {
+            if (name.equals("http.server.requests") && context instanceof ServerRequestObservationContext serverContext) {
+                return !serverContext.getCarrier().getRequestURI().startsWith("/actuator");
             }
-
-            @Override
-            public String getDescription() {
-                return "CustomSamplerForActuator";
-            }
+            return true;
         };
     }
 
